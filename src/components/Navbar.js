acf25c91d78ea038e15de23aae17f455b6cf1986
Diana Mora <morra.dianna@gmail.com>
2021-05-14 16:26:43 -0400

    trying to make navbar mobile friendly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click); // reversing the state so the icon switches back and forth when click
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+    window.addEventListener('resize', showButton);
 
     return (
         <div>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to="/" className='navbar-logo'>
+                    <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
                         B.O.B. <i class='fas fa-beer'></i>
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
