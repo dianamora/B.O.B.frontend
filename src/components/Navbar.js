010c05d86850b1c9773c3b091f9e414b813090b0
Diana Mora <morra.dianna@gmail.com>
2021-05-14 15:46:00 -0400

    added menu icon and state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,17 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const [click, setClick] = useState(false)
     return (
         <div>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        B.O.B. <i class="fas fa-beer"></i>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to="/" className='navbar-logo'>
+                        B.O.B. <i class='fas fa-beer'></i>
                     </Link>
+                    <div className='menu-icon'>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
                 </div>
 
             </nav>
