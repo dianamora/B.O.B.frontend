74ee5172dce718c123b830850d38ed41418765bd
Diana Mora <morra.dianna@gmail.com>
2021-05-14 20:54:38 -0400

    beginning to work on form that will post to the api

---
+++ src/components/Contributeform.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class Contributeform extends React.Component {
+    render() {
+        return (
+            <form>
+                <label>Name:</label>
+                <input type="text" name="name" onChange={ } />
+                {/* location */}
+                <label>Name:</label>
+                <input type="text" name="name" onChange={ } />
+                {/* hours */}
+                <label>Name:</label>
+                <input type="text" name="name" onChange={ } />
+                {/* contact */}
+                <label>Name:</label>
+                <input type="text" name="name" onChange={ } />
+                {/* website */}
+                <label>Name:</label>
+                <input type="text" name="name" onChange={ } />
+                <input type="submi" value="Add Brewery" />
+            </form>
+        )
+    }
+}
